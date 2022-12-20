9d551c0d1290a2d0e0466dae69ec47ebb94dec69
Akshata Kanaje <akshatakanaje@Akshatas-MacBook-Pro.local>
2022-12-20 15:14:41 -0800

    added admins pojo class

---
+++ src/main/java/com/simplilearn/estorezone/admin/model/Admins.java
@@ -0,0 +1,87 @@
+package com.simplilearn.estorezone.admin.model;
+
+import java.util.Date;
+
+public class Admins {
+	
+	private int adminId;
+	private String email;
+	private String password;
+	private String fullName;
+	private int longType;
+	private Date addedOn;
+	
+	//default constructor
+	public Admins() {
+		
+	}
+    
+	//parameterized constructor
+	public Admins(int adminId, String email, String password, String fullName, int longType, Date addedOn) {
+		super();
+		this.adminId = adminId;
+		this.email = email;
+		this.password = password;
+		this.fullName = fullName;
+		this.longType = longType;
+		this.addedOn = addedOn;
+	}
+    
+	//getter and setter methods
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public int getLongType() {
+		return longType;
+	}
+
+	public void setLongType(int longType) {
+		this.longType = longType;
+	}
+
+	public Date getAddedOn() {
+		return addedOn;
+	}
+
+	public void setAddedOn(Date addedOn) {
+		this.addedOn = addedOn;
+	}
+
+	
+	//override to string
+	@Override
+	public String toString() {
+		return "Admins [adminId=" + adminId + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+				+ ", longType=" + longType + ", addedOn=" + addedOn + "]";
+	}	
+
+}
